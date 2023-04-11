6cc2287efe5bf515b88533a5a4a2a60c261f39c2
szunami <sam.szuflita@gmail.com>
2023-04-11 12:19:43 -0400

    Feels a lot like tetris

--- client/src/scenes/GameScene.ts
+++ client/src/scenes/GameScene.ts
@@ -87,36 +87,25 @@ export class GameScene extends Scene {
     }, 1000);
 
     // Handle keyboard input
-    // const keys = this.input.keyboard.addKeys("W,S,A,D") as {
-    //   W: Phaser.Input.Keyboard.Key;
-    //   S: Phaser.Input.Keyboard.Key;
-    //   A: Phaser.Input.Keyboard.Key;
-    //   D: Phaser.Input.Keyboard.Key;
-    // };
-    // let prevDirection = Direction.None;
+    const keys = this.input.keyboard.addKeys("W,S,A,D") as {
+      W: Phaser.Input.Keyboard.Key;
+      S: Phaser.Input.Keyboard.Key;
+      A: Phaser.Input.Keyboard.Key;
+      D: Phaser.Input.Keyboard.Key;
+    };
 
-    // const handleKeyEvt = () => {
-    //   let direction: Direction;
-    //   if (keys.W.isDown) {
-    //     direction = Direction.Up;
-    //   } else if (keys.S.isDown) {
-    //     direction = Direction.Down;
-    //   } else if (keys.D.isDown) {
-    //     direction = Direction.Right;
-    //   } else if (keys.A.isDown) {
-    //     direction = Direction.Left;
-    //   } else {
-    //     direction = Direction.None;
-    //   }
+    const handleKeyEvt = () => {
+      if (keys.W.isDown) {
+      } else if (keys.S.isDown) {
+      } else if (keys.D.isDown) {
+        this.connection.sendMessage({ type: ClientMessageType.MoveRight });
+      } else if (keys.A.isDown) {
+        this.connection.sendMessage({ type: ClientMessageType.MoveLeft });
+      } else {
+      }
+    };
 
-    //   if (prevDirection !== direction) {
-    //     // If connection is open and direction has changed, send updated direction
-    //     prevDirection = direction;
-    //     this.connection.sendMessage({ type: ClientMessageType.SetDirection, direction });
-    //   }
-    // };
-
-    // this.input.keyboard.on("keydown", handleKeyEvt);
+    this.input.keyboard.on("keydown", handleKeyEvt);
   }
 
   update() {
@@ -127,8 +116,6 @@ export class GameScene extends Scene {
 
     const { state } = this.stateBuffer.getInterpolatedState(Date.now());
 
-    console.log(state);
-
     if (state.player1 !== undefined && this.player1Text === undefined) {
       this.player1Text = this.add.text(450, 0, `${state.player1.id}`, { color: "white" }).setScrollFactor(0);
     }
@@ -138,10 +125,8 @@ export class GameScene extends Scene {
     }
 
     // todo: clear sprites from cleared bricks
-
     state.bricks.forEach((brick) => {
       if (!this.existingBricks.has(brick.id)) {
-        console.log("Drawing", brick);
         this.existingBricks.set(brick.id, this.add.sprite(this.x(brick.point.x), this.y(brick.point.y), "grass"));
       }
     });
@@ -156,40 +141,17 @@ export class GameScene extends Scene {
       }
     });
 
-    // // Synchronize the players in our game's state with sprites to represent them graphically
-    // this.syncSprites(
-    //   this.players,
-    //   new Map(
-    //     state.players.map((player) => [
-    //       player.id,
-    //       new Phaser.GameObjects.Sprite(this, player.position.x, player.position.y, "player").setRotation(
-    //         player.aimAngle
-    //       ),
-    //     ])
-    //   )
-    // );
-
-    // // Do the same with bullets
-    // this.syncSprites(
-    //   this.bullets,
-    //   new Map(
-    //     state.bullets.map((bullet) => [
-    //       bullet.id,
-    //       new Phaser.GameObjects.Sprite(this, bullet.position.x, bullet.position.y, "bullet"),
-    //     ])
-    //   )
-    // );
-
-    // // If this.playerSprite has been defined (a ref to our own sprite), send our mouse position to the server
-    // if (this.playerSprite) {
-    //   this.sendMousePosition(mousePointer, this.playerSprite);
-    // }
+    state.player2Falling?.bricks.forEach((brick) => {
+      if (!this.existingBricks.has(brick.id)) {
+        this.existingBricks.set(brick.id, this.add.sprite(this.x(brick.point.x), this.y(brick.point.y), "grass"));
+      } else {
+        const existingBrick = this.existingBricks.get(brick.id);
+        existingBrick?.setX(this.x(brick.point.x));
+        existingBrick?.setY(this.y(brick.point.y));
+      }
+    });
   }
 }
 function lerp(from: GameState, to: GameState, pctElapsed: number): GameState {
   return to;
-}
-
-function key(brick: Brick): string {
-  return `(${brick.point.x}, ${brick.point.y}`;
 }
--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 
 import { UserId, RoomId, Application, startServer, verifyJwt } from "@hathora/server-sdk";
 import dotenv from "dotenv";
-import { GameState } from "../common/types";
+import { GameState, Point, Tetronimo } from "../common/types";
 import { ClientMessage, ClientMessageType, ServerMessage, ServerMessageType } from "../common/messages";
 import { V4MAPPED } from "dns";
 
@@ -28,8 +28,9 @@ const store: Application = {
   subscribeUser(roomId: RoomId, userId: string): void {
     if (!rooms.has(roomId)) {
       console.log("newRoom", roomId, userId);
+      // todo: interesting initial config
       rooms.set(roomId, {
-        bricks: [{ point: { x: 0, y: 0 }, id: uuidv4() }],
+        bricks: [{ point: { x: 0, y: 10 }, id: uuidv4() }],
         player1: {
           id: userId
         },
@@ -43,7 +44,11 @@ const store: Application = {
         game.player2 = { id: userId };
 
         game.player1Falling = {
-          bricks: [{ point: { x: 5, y: 0 }, id: uuidv4() }]
+          bricks: [{ point: { x: 0, y: 0 }, id: uuidv4() }]
+        };
+
+        game.player2Falling = {
+          bricks: [{ point: { x: 0, y: 20 }, id: uuidv4() }]
         };
       }
     }
@@ -70,25 +75,67 @@ const store: Application = {
     // Parse out the data string being sent from the client
     const message: ClientMessage = JSON.parse(Buffer.from(data).toString("utf8"));
 
-    // Handle the various message types, specific to this game
-    // if (message.type === ClientMessageType.SetDirection) {
-    //   player.direction = message.direction;
-    // } else if (message.type === ClientMessageType.SetAngle) {
-    //   player.angle = message.angle;
-    // } else if (message.type === ClientMessageType.Shoot) {
-    //   const body = game.physics.createCircle({ x: player.body.x, y: player.body.y }, BULLET_RADIUS);
-    //   game.bullets.push({
-    //     id: Math.floor(Math.random() * 1e6),
-    //     body: Object.assign(body, { oType: BodyType.Bullet }),
-    //     angle: player.angle,
-    //   });
-    // } else if (message.type === ClientMessageType.Ping) {
-    //   const msg: ServerMessage = {
-    //     type: ServerMessageType.PingResponse,
-    //     id: message.id,
-    //   };
-    //   server.sendMessage(roomId, userId, Buffer.from(JSON.stringify(msg), "utf8"));
-    // }
+    if (message.type === ClientMessageType.Ping) {
+      const msg: ServerMessage = {
+        type: ServerMessageType.PingResponse,
+        id: message.id,
+      };
+      server.sendMessage(roomId, userId, Buffer.from(JSON.stringify(msg), "utf8"));
+    } else {
+
+      const occupiedPoints = new Set<string>();
+      game.bricks.forEach(brick => {
+        occupiedPoints.add(key(brick.point));
+      });
+
+      if (userId === game.player1?.id) {
+        // Handle the various message types, specific to this game
+        if (message.type === ClientMessageType.MoveRight) {
+          // todo: check if blocked
+          var rightBlocked = false;
+
+          game.player1Falling?.bricks.forEach(brick => {
+            if (occupiedPoints.has(key({ x: brick.point.x + 1, y: brick.point.y }))
+              || brick.point.x + 1 == 10
+            ) {
+              rightBlocked = true;
+            }
+          });
+
+          if (!rightBlocked) {
+            game.player1Falling?.bricks.forEach(brick => {
+              brick.point.x = brick.point.x + 1;
+            });
+          }
+        }
+
+        if (message.type === ClientMessageType.MoveLeft) {
+          // todo: check if blocked
+          var leftBlocked = false;
+
+          game.player1Falling?.bricks.forEach(brick => {
+            if (occupiedPoints.has(key({ x: brick.point.x - 1, y: brick.point.y }))
+              || brick.point.x - 1 == -1) {
+              leftBlocked = true;
+            }
+          });
+
+          if (!leftBlocked) {
+            game.player1Falling?.bricks.forEach(brick => {
+              brick.point.x = brick.point.x - 1;
+            });
+          }
+        }
+
+        // todo: remove player2s update here!
+        const msg: ServerMessage = {
+          type: ServerMessageType.StateUpdate,
+          state: game,
+          ts: Date.now(),
+        }
+        server.sendMessage(roomId, userId, Buffer.from(JSON.stringify(msg), "utf8"));
+      }
+    }
   },
 };
 
@@ -114,14 +161,56 @@ setInterval(() => {
   });
 }, TICK_INTERVAL_MS);
 
+
+function key(point: Point): string {
+  return `(${point.x}, ${point.y}`;
+}
+
 // The frame-by-frame logic of your game should live in it's server's tick function. This is often a place to check for collisions, compute score, and so forth
 function tick(game: GameState, deltaMs: number) {
 
+  const occupiedPoints = new Set<string>();
+  game.bricks.forEach(brick => {
+    occupiedPoints.add(key(brick.point));
+  })
+
   if (game.player1 !== undefined && game.player2 !== undefined) {
+
+    var player1Blocked = false;
     game.player1Falling?.bricks.forEach((brick) => {
-      brick.point.y = brick.point.y - 1;
+      if (occupiedPoints.has(key({ x: brick.point.x, y: brick.point.y + 1 }))) {
+        player1Blocked = true;
+      }
+    });
+    if (!player1Blocked) {
+      game.player1Falling?.bricks.forEach((brick) => {
+        brick.point.y = brick.point.y + 1;
+      })
+    }
+    if (player1Blocked) {
+      game.player1Falling?.bricks.forEach((brick) => {
+        game.bricks.push(brick);
+      });
+      game.player1Falling = player1line();
+    }
+
+    game.player2Falling?.bricks.forEach((brick) => {
+      if (!occupiedPoints.has(key({ x: brick.point.x, y: brick.point.y - 1 }))) {
+        brick.point.y = brick.point.y - 1;
+      }
     });
   }
+}
+
+function player1line(): Tetronimo {
+  return {
+    bricks: [
+      { point: { x: 0, y: 0 }, id: uuidv4() },
+      { point: { x: 1, y: 0 }, id: uuidv4() },
+      { point: { x: 2, y: 0 }, id: uuidv4() },
+      { point: { x: 3, y: 0 }, id: uuidv4() }
+    ]
+  };
 }
 
 function broadcastStateUpdate(roomId: RoomId) {
